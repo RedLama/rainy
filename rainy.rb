0f2c3486b8a61c919ef0a53405b3de88b5022f8f
Damien <damien@redlama.fr>
2015-04-01 20:35:34 +0200

    display result in a human readable way

--- rainy.rb
+++ rainy.rb
@@ -7,28 +7,39 @@ require 'json'
 #database file
 GC_SQLITE_FILE = 'GC_FR.sqlite'
 
-RAIN_LEVEL = { 1 => "Aucune",
-  2 => "pluie faible",
+RAIN_LEVEL = { 2 => "pluie faible",
   3 => "pluie modérée",
   4 => "forte pluie"}
 
+def humanize_result(result)
+  first = result.shift
+  sentence = first["index"] == 0 ?
+    "La #{first[:rain_level]} " << first[:duration] == 60 ? "ne s'arretera pas tout de suite" : "s'arretera dans #{first[:duration]} minutes" :
+     sprintf("Il y aura dans %d minutes une %s pendant %d minutes", first.values) if first
+
+  result.each do |state|
+    sentence << "il y aura #{state[:beginning] - first[:index] + first[:duration]} minutes après une #{state[:rain_level]} pendant #{state[:duration]} minutes"
+    first = state
+  end
+  result.join(", ")
+end
+
 def extract_information(data)
-  data["dataCadran"].to_enum.with_index.map { |d, index|
+  data["dataCadran"].to_enum.with_index.map do |d, index|
     d['index'] = index
     d
-  }.chunk { |d| d["niveauPluie"] }.map { |level, blocs|
-    "Il y aura dans #{blocs.first["index"]} minutes une #{RAIN_LEVEL[level]} pendant #{blocs.count * 5} minutes" if level > 1
+  end.chunk { |d| d["niveauPluie"] }.map { |level, blocs|
+    { beginning: blocs.first["index"] * 5, rain_level: RAIN_LEVEL[level], duration: blocs.count * 5 } if level > 1
   }.compact
 end
 
-def display_information(data)
+def parse_information(data)
   if !data["isAvailable"]
     p "Information non disponible pour votre département"
   elsif !data["hasData"]
     p "Pas de donnée, réessayer plus tard"
   else
-    toto = extract_information data
-    ap toto
+    p humanize_result(extract_information( data ))
   end
 end
 
@@ -39,7 +50,7 @@ begin
   sql_db.execute('SELECT code_departement, code_commune, article_riche, nom_riche FROM french_geographic_codes WHERE code_postal = ?', "78180") do |row|
     uri = URI("http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/#{row[0] + row[1] + '0'}")
     res = Net::HTTP.get_response(uri)
-    display_information(JSON.parse(res.body)) if res.is_a?(Net::HTTPSuccess)
+    parse_information(JSON.parse(res.body)) if res.is_a?(Net::HTTPSuccess)
   end
 rescue SQLite3::Exception => e
   puts "Exception occurred"
