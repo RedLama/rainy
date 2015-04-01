8487060ff0a39ca83d2e9246886229cc0d21fb15
Damien <damien@redlama.fr>
2015-04-02 01:22:28 +0200

    do it as a script

--- rainy.rb
+++ rainy.rb
@@ -1,8 +1,10 @@
+#!/usr/bin/env ruby
 require 'sqlite3'
 require 'awesome_print'
 
 require 'net/http'
 require 'json'
+require 'optparse'
 
 #database file
 GC_SQLITE_FILE = 'GC_FR.sqlite'
@@ -46,20 +48,21 @@ def parse_information(data)
   end
 end
 
+if __FILE__ == $0
+  begin
+    # Opening sqlite Database
+    sql_db = SQLite3::Database.new(GC_SQLITE_FILE)
 
-begin
-  # Opening sqlite Database
-  sql_db = SQLite3::Database.new(GC_SQLITE_FILE)
-
-  sql_db.execute('SELECT code_departement, code_commune, article_riche, nom_riche FROM french_geographic_codes WHERE code_postal = ?', "78180") do |row|
-    uri = URI("http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/#{row[0] + row[1] + '0'}")
-    res = Net::HTTP.get_response(uri)
-    parse_information(JSON.parse(res.body)) if res.is_a?(Net::HTTPSuccess)
+    sql_db.execute('SELECT code_departement, code_commune, article_riche, nom_riche FROM french_geographic_codes WHERE code_postal = ?', "78180") do |row|
+      uri = URI("http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/#{row[0] + row[1] + '0'}")
+      res = Net::HTTP.get_response(uri)
+      parse_information(JSON.parse(res.body)) if res.is_a?(Net::HTTPSuccess)
+    end
+  rescue SQLite3::Exception => e
+    puts "Exception occurred"
+    puts e
+  ensure
+    #Disconnecting Databse
+    sql_db.close if sql_db
   end
-rescue SQLite3::Exception => e
-  puts "Exception occurred"
-  puts e
-ensure
-  #Disconnecting Databse
-  sql_db.close if sql_db
 end
